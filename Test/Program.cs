bd6d875a49dd7f6b71301fbad2cffa93b2f7ff6d
吉吉桑 <406992538@qq.com>
2022-03-31 16:19:50 +0800

    测试

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,15 +9,15 @@ using Test;
 Console.WriteLine("Hello, World!");
 IExpressionConvert expressionConvert = new ExpressionConvert();
 IExpressionCalculator calculator = new ExpressionCalculator(expressionConvert);
-//var exp = "测试.测试算法1`测试.测试算法2";
-//var result = await calculator.CalcAsync(new TestExpressionParam
-//{
-//    Expression = exp,
-//    Ratio = 2,
-//    DynamicVariables = new List<ExpressionVariableParam> {
-//    new ExpressionVariableParam("a","1") }
-//});
-//Console.WriteLine($"{exp} = {result}");
+var exp = "测试.测试算法1+测试.测试算法2";
+var result = await calculator.CalcAsync(new TestExpressionParam
+{
+    Expression = exp,
+    Ratio = 2,
+    DynamicVariables = new List<ExpressionVariableParam> {
+    new ExpressionVariableParam("a","1") }
+});
+Console.WriteLine($"{exp} = {result}");
 //var exp2 = "sin30-((10-测试.测试算法1)addmul2(测试.测试算法2+1)^2)+sin30";
 //var result2 = await calculator.CalcAsync(new TestExpressionParam { Expression = exp2, Ratio = 2 });
 //Console.WriteLine($"{exp2} = {result2}");
